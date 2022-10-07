df7e1c117c82074f20c2ce4e8fa62a9729e2957e
JuanDomínguez <juandominortega92@gmail.com>
2022-10-07 19:12:12 +0200

    Initial exercises

--- js/app.js
+++ js/app.js
@@ -1,24 +1,19 @@
 // //Determinar el tipo de dato y valor de las variables
 // // Caso a
-// let variable = 883 < 542;
+ let variable = 883 < 542;
 // console.log("a)tipo:",typeof variable, "valor:", variable);
 // //Caso b
 // variable  = 17 / 5;
 // console.log("b)tipo:",typeof variable, "valor:", variable);
 
-/* let totalBet = prompt("please enter your bet");
-/* console.log(totalBet); */
+let totalBet = prompt("please enter your bet");
+/* console.log("totalBet"); */
 
-/* if (totalBet == 5000) {
+if (totalBet == 5000) {
   alert("all-inn!");
 } else if (totalBet < 5000){ 
   alert("passing this round!");
 } else{
     alert("wrong bet, try again");
     totalBet = prompt("please enter your bet");
-} */ 
- let number = 10;
- while (number > 0){
-  number--;
-  console.log(number);
- }
+}
